0e94ef8bb8d1f23fa4adced08060bfe7fbae6110
ZaneN8 <zanelnewsom@gmail.com>
2020-08-20 12:08:31 -0600

    changed it again

--- main.rb
+++ main.rb
@@ -1,1 +1,11 @@
-puts "Hello"
+def add(num1, num2)
+  num1 + num2
+end
+
+# this is a minus
+def minus(num1, num2)
+  num1 - num2
+end
+
+puts add(1,1)
+puts minus(1,1)
---
+++ test.rb
@@ -0,0 +1,10 @@
+def add(num1, num2)
+  num1 + num2
+end
+
+def minus(num1, num2)
+  num1 - num2
+end
+
+puts add(1,1)
+puts minus(1,1)
